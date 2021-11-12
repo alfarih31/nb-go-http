1dfc9b859f5ce93b34206f03c23613e6cfd0bb04
Alfarih Faza <alfarihfz@gmail.com>
2021-11-12 16:04:10 +0700

    feat: added types is_equal

---
+++ types/is_equal.go
@@ -0,0 +1,25 @@
+package types
+
+import "reflect"
+
+func IsTypeEqual(v1 interface{}, v2 interface{}) bool {
+	t1 := reflect.TypeOf(v1)
+	t2 := reflect.TypeOf(v2)
+	if t1 == nil {
+		if t2 == nil {
+			return true
+		}
+
+		return false
+	}
+
+	if t1.Name() != t2.Name() {
+		return false
+	}
+
+	if t1.Kind() != t2.Kind() {
+		return false
+	}
+
+	return true
+}
